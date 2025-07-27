a6dee8a83fd9337a524b0b4334e53e543e1fad95
meilirensheng2020 <yansp@zetyun.com>
2025-07-27 19:54:09 +0800

    [refact] Refact rpc retry errors

--- pkg/base/rpc_status.go
+++ pkg/base/rpc_status.go
@@ -20,6 +20,30 @@ import (
 	"github.com/dingodb/dingofs-tools/proto/dingofs/proto/metaserver"
 )
 
+var (
+	metaServerRetryErrors = map[metaserver.MetaStatusCode]bool{
+		metaserver.MetaStatusCode_UNKNOWN_ERROR:    true,
+		metaserver.MetaStatusCode_RPC_ERROR:        true,
+		metaserver.MetaStatusCode_REDIRECTED:       true,
+		metaserver.MetaStatusCode_OVERLOAD:         true,
+		metaserver.MetaStatusCode_RPC_STREAM_ERROR: true,
+	}
+	mdsRetryErrors = map[mds.FSStatusCode]bool{
+		mds.FSStatusCode_UNKNOWN_ERROR: true,
+		mds.FSStatusCode_RPC_ERROR:     true,
+		mds.FSStatusCode_FS_BUSY:       true,
+		mds.FSStatusCode_LOCK_TIMEOUT:  true,
+	}
+	mdsV2RetryErrors = map[mdsv2error.Errno]bool{
+		mdsv2error.Errno_EREQUEST_FULL:      true,
+		mdsv2error.Errno_EGEN_FSID:          true,
+		mdsv2error.Errno_EREDIRECT:          true,
+		mdsv2error.Errno_ENOT_SERVE:         true,
+		mdsv2error.Errno_EPARTIAL_SUCCESS:   true,
+		mdsv2error.Errno_ESTORE_MAYBE_RETRY: true,
+	}
+)
+
 type MetaServerStatusChecker interface {
 	GetStatusCode() metaserver.MetaStatusCode
 }
@@ -33,14 +57,28 @@ type MdsV2StatusChecker interface {
 }
 
 func CheckRpcNeedRetry(result interface{}) bool {
+	// check metaServer retry errors
 	if checker, ok := result.(MetaServerStatusChecker); ok {
-		return checker.GetStatusCode() != metaserver.MetaStatusCode_OK
+		statusCode := checker.GetStatusCode()
+		if _, exists := metaServerRetryErrors[statusCode]; exists {
+			return true
+		}
 	}
+
+	// check mds retry errors
 	if checker, ok := result.(MdsStatusChecker); ok {
-		return checker.GetStatusCode() != mds.FSStatusCode_OK
+		statusCode := checker.GetStatusCode()
+		if _, exists := mdsRetryErrors[statusCode]; exists {
+			return true
+		}
 	}
+
+	// check mdsV2 retry errors
 	if checker, ok := result.(MdsV2StatusChecker); ok {
-		return checker.GetError().GetErrcode() != mdsv2error.Errno_OK
+		errCode := checker.GetError().GetErrcode()
+		if _, exists := mdsV2RetryErrors[errCode]; exists {
+			return true
+		}
 	}
 
 	return false
